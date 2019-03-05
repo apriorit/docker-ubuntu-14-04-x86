8f4811c26515e57e923fd2e7d885e79616a05a52
koshel.eugene <koshel.eugene@apriorit.com>
2019-03-05 12:51:29 +0200

    Qt updated to 4.8.7, patches are removed as obsolete.

--- docker/install-qt.sh
+++ docker/install-qt.sh
@@ -40,7 +40,7 @@ function check_hash() {
 }
 
 
-QT_SOURCE="qt-everywhere-opensource-src-4.8.1"
+QT_SOURCE="qt-everywhere-opensource-src-4.8.7"
 QT_BUILD_DIR=/tmp/qt
 QT_INSTALL_DIR=/opt/Qt
 
@@ -52,8 +52,6 @@ QTDIR=$QT_INSTALL_DIR
 PATH=$QTDIR/bin:$PATH
 
 FAST=-j$(expr $(cpu_count) + 1)
-
-PATCH_DIR=/tmp/docker
 
 # Install build prerequisites
 # ===========================
@@ -79,10 +77,10 @@ package_install \
 # ======================================
 
 cd $QT_BUILD_DIR
-wget http://download.qt.io/archive/qt/4.8/4.8.1/$QT_SOURCE.tar.gz
-check_hash sha256sum $QT_SOURCE.tar.gz "ef851a36aa41b4ad7a3e4c96ca27eaed2a629a6d2fa06c20f072118caed87ae8"
-check_hash sha1sum   $QT_SOURCE.tar.gz "a074d0f605f009e23c63e0a4cb9b71c978146ffc"
-check_hash md5sum    $QT_SOURCE.tar.gz "7960ba8e18ca31f0c6e4895a312f92ff"
+wget http://download.qt.io/archive/qt/4.8/4.8.7/$QT_SOURCE.tar.gz
+check_hash sha256sum $QT_SOURCE.tar.gz "e2882295097e47fe089f8ac741a95fef47e0a73a3f3cdf21b56990638f626ea0"
+check_hash sha1sum   $QT_SOURCE.tar.gz "76aef40335c0701e5be7bb3a9101df5d22fe3666"
+check_hash md5sum    $QT_SOURCE.tar.gz "d990ee66bf7ab0c785589776f35ba6ad"
 tar -zxf $QT_SOURCE.tar.gz
 
 cd $QT_BUILD_DIR/shadow
@@ -91,28 +89,6 @@ $QT_BUILD_DIR/$QT_SOURCE/configure -prefix $QT_INSTALL_DIR -platform $QT_PLATFOR
     -no-qt3support -webkit -no-javascript-jit -silent -continue -plugin-sql-sqlite \
     -plugin-sql-sqlite2 -qt-zlib -qt-libtiff -qt-libpng -qt-libmng -qt-libjpeg \
     -confirm-license
-
-# Apply our patches onto Qt
-# =========================
-
-cd $QT_BUILD_DIR/$QT_SOURCE
-
-patch -p1 --input="$PATCH_DIR/qt-0001-webkit-disable-javascript-jit.patch"
-
-# WebKit uses some forward declarations of Glib objects which are not kept up to date.
-# In this case, Glib has updated GMutex to be a union, but JSC thinks of it as a struct.
-# Check whether the old definition works, if it doesn't then update WebKit definitions.
-if ! gcc -fsyntax-only $(pkg-config --cflags glib-2.0) -x c - 2>&1 >/dev/null <<EOF
-#include <glib.h>
-typedef struct _GMutex _GMutex;
-EOF
-then
-    patch -p1 --input="$PATCH_DIR/qt-0002-webkit-update-gobject-forward-declarations.patch"
-fi
-
-# Contrary to what is said in QtWebKit changelog, -Werror does *not* pass on x86
-# with gcc 4.6 due to silly errors. Disable it to avoid breaking the build.
-patch -p1 --input="$PATCH_DIR/qt-0003-webkit-disable-warnings-as-errors.patch"
 
 # Build and install
 # =================
